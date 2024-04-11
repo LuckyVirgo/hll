0ca76f93feade7b0b346575f1714492f89891270
LuckyVirgo <123327190+LuckyVirgo@users.noreply.github.com>
2024-04-12 01:17:56 +0800

    Update xmind.js

--- xmind.js
+++ xmind.js
@@ -22,14 +22,14 @@ if (vip.test($request.url)) {
     chxm1023 = {
         "license" : {
             "status" : "sub",
-            "expireTime" : 253402185599000
+            "expireTime" : 253402128000000
         },
         "_code" : 200,
         "raw_data" : "GfxYgAqnrQ\/ggD9UwqnZyAj6FKnopXzM8s5m3eZLOsmVr4FwCzv41qTmgi\/u72cv+jpaAoljaEPti1twzOS7X7KUPY1KNJ2xalbS7SXbvFHSvs21QXjaUtIOkeJWAl4\/vHl4IvMeHTBVqD8mFCXOmvnBPLRMAJEPgHEJYF1InvQ="};
 }
 
 if (token.test($request.url)) {
-    chxm1023.expireDate = 253402185599;
+    chxm1023.expireDate = 253402128000;
     chxm1023.token = "f50633ea8eb04cbb85962b99c47045d7AjOobEGo";
 }
 
